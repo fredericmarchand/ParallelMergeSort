2627743d140fb213cac163084e8debaaa0808dde
Frederic Marchand <fred.marchand.23@gmail.com>
2014-10-15 20:19:46 -0400

    Working with dynamic allocation

--- main.cpp
+++ main.cpp
@@ -14,30 +14,38 @@
 #include <stdlib.h>
 using namespace std;
 
+#define NOALLOC 1 
+
 #define DEBUG 0 
+#if NOALLOC == 1
+#define ARRAY_A 1
+#define ARRAY_B 2
+#endif
 
 int *inputArray;
 int *outputArray;
 
-int binarySearch(int x, int *array, int upperBound)
+int binarySearch(int x, int *array, int low, int high)
 {
-    int lowerBound = 0;
-    int position = (upperBound / 2);
-
-    while ((array[position] != x) && (lowerBound <= upperBound))
+    
+    if (high < low)
     {
-        if (array[position] > x)
-        {
-            upperBound = position - 1;
-        }
-        else
-        {
-            lowerBound = position + 1;
-        }
-        position = (lowerBound + upperBound) / 2;
+        return low;
+    }
+    int mid = (low + high) / 2;
+    if (array[mid] > x)
+    {
+        return binarySearch(x, array, low, mid-1);
+    }
+    else if (array[mid] < x)
+    {
+        return binarySearch(x, array, mid+1, high);
+    }
+    else
+    {
+        return mid;
     }
 
-    return position;
 }
 
 void parallelMerge(int *C, int *A, int *B, int na, int nb)
@@ -46,29 +54,23 @@ void parallelMerge(int *C, int *A, int *B, int na, int nb)
     {
         cilk_spawn parallelMerge(C, B, A, nb, na);
     }
-    else if (na == 1)
+    else if (na == 0)
     {
-        if (nb == 0)
-        {
-            C[0] = A[0];
-        }
-        else
-        {
-            C[0] = (A[0] < B[0]) ? A[0] : B[0];
-            C[1] = (A[0] < B[0]) ? B[0] : A[0];
-        }
+        return;
     }
     else
     {
         int ma = na/2;
-        int mb = binarySearch(A[ma], B, nb);
+        int mb = binarySearch(A[ma], B, 0, nb-1);
+        C[ma + mb] = A[ma];
         cilk_spawn parallelMerge(C, A, B, ma, mb);
-        cilk_spawn parallelMerge(C+ma+mb, A+ma, B+mb, na-ma, nb-mb);
+        parallelMerge(C+ma+mb+1, A+ma+1, B+mb, na-ma-1, nb-mb);
         cilk_sync;
     }
 
 }
 
+#if NOALLOC == 0
 void parallelMergeSort(int *B, int *A, int n)
 {
     if (n == 1)
@@ -77,20 +79,47 @@ void parallelMergeSort(int *B, int *A, int n)
     }
     else
     {
-        int *C = (int *)malloc(n * sizeof(int));
-        cilk_spawn parallelMergeSort(C, A, n/2);
-        cilk_spawn parallelMergeSort(C+n/2, A+n/2, n-n/2); 
+        int *C = (int *) malloc(n * sizeof(int));
+        cilk_spawn parallelMergeSort(C,     A,     n/2);
+        parallelMergeSort(C+n/2, A+n/2, n-n/2); 
         cilk_sync;
         parallelMerge(B, C, C+n/2, n/2, n-n/2);
-        free(C);
+        cilk_sync;
+        free (C);
     }
 }
+#endif
+
+#if NOALLOC == 1
+void parallelMergeSort(int *A, int *B, int n, int AorB)
+{
+    if (n <= 1)
+    {
+        B[0] = A[0];
+    }
+    
+    if (AorB == ARRAY_A)
+    {
+        cilk_spawn parallelMergeSort(A, B, n/2, ARRAY_B);
+        parallelMergeSort(A+n/2, B+n/2, n-n/2, ARRAY_B); 
+        cilk_sync;
+        parallelMerge(A, B, B+n/2, n/2, n-n/2);
+    }
+    else
+    {
+        cilk_spawn parallelMergeSort(A, B, n/2, ARRAY_A);
+        parallelMergeSort(A+n/2, B+n/2, n-n/2, ARRAY_A); 
+        cilk_sync;
+        parallelMerge(B, A, A+n/2, n/2, n-n/2);
+    }
+}
+#endif
 
 void printArray(int n)
 {
     for (int i = 0; i < n; ++i)
     {
-        cout << inputArray[i] << " " << endl;
+        cout << outputArray[i] << " " << endl;
     }
 }
 
@@ -152,13 +181,20 @@ int main(int argc, char** argv)
     time1 = __cilkview_getticks();
 
     // Run Merge Sort
-
+#if NOALLOC == 1
+    parallelMergeSort(inputArray, outputArray, n, ARRAY_A);
+#else
     parallelMergeSort(outputArray, inputArray, n);
+#endif
 
     time2 = __cilkview_getticks();
 
     par_time = time2-time1;
     cout << "\nParallel Merge Sort took " << par_time << " milliseconds." << endl;
+
+#if DEBUG == 2 
+    printArray(n);
+#endif
 
     /// Write to output file
     ofstream outputFile (outputFilepath);
@@ -171,7 +207,11 @@ int main(int argc, char** argv)
     outputFile << n << endl;
     for (int i = 0; i < n; ++i)
     {
+#if NOALLOC == 1
+        outputFile << inputArray[i] << endl;
+#else 
         outputFile << outputArray[i] << endl;
+#endif
     }
 
     outputFile.close();
